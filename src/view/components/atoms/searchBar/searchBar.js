cc06a36fd92f635f062ac07b589b8f0149a563d6
Nathan Neelis <nathan.neelis@wearetriple.com>
2022-09-28 23:30:42 +0200

    background map interactive

---
+++ src/view/components/atoms/searchBar/searchBar.js
@@ -0,0 +1,11 @@
+import $ from "./searchBar.module.scss";
+
+function SearchBar(props) {
+  return (
+    <div className={$.searchBar}>
+      <h2>something</h2>
+    </div>
+  );
+}
+
+export default SearchBar;
