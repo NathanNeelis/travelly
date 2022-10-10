54f92c46e2e972f892faffa62572cdf7c55b2140
Nathan Neelis <nathan.neelis@wearetriple.com>
2022-10-10 20:25:10 +0200

    dynamic result cards

--- src/view/components/organisms/discover/discover.jsx
+++ src/view/components/organisms/discover/discover.jsx
@@ -5,6 +5,35 @@ import Filters from "../../molecules/filters/filters";
 import Results from "../../molecules/results/results";
 
 function Discover(props) {
+  let fakeData = [
+    {
+      locality: "a very long locality title",
+      country: "United Kingdom",
+      tags: [
+        {
+          winter: "Winter Wonderland",
+          summer: "Mild summer",
+        },
+      ],
+      slug: "123AdBDC",
+      image:
+        "https://media-cdn.tripadvisor.com/media/vr-splice-j/05/63/b5/09.jpg",
+    },
+    {
+      locality: "a very long locality title",
+      country: "Italy",
+      tags: [
+        {
+          winter: "Frisky",
+          summer: "Scorching heat",
+        },
+      ],
+      slug: "123AdBaSDDC",
+      image:
+        "https://www.francecomfort.com/l/nl/library/download/urn:uuid:a3713af6-e347-4f43-b78a-7ad53eb75766/via+ferrata+4+frankrijk+haute+savoie+vakantie+luxe+appartement+portes+du+soleil+bergen+klimmen+wandelen.jpg?scaleType=3&width=1600&height=1000",
+    },
+  ];
+
   return (
     <div className={$.discover}>
       <SectionHeader section={"Discover"} subtitle={"Where to next?"} />
@@ -15,7 +44,7 @@ function Discover(props) {
 
       <div className={$.filter}>
         <Filters />
-        <Results />
+        <Results data={fakeData} />
 
         {/* todo: add props as results  */}
       </div>
